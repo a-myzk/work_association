08dd328b84040701ec9b80cebf3cf3f618f31ebe
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-04-26 17:38:06 +0900

    不要なルート記述を削除

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :favorites, only: [:create, :destroy]
-  root to: "favorites#index"
 end
+
