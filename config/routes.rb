3b4ed3b0aa843b28030ae910dfa44b39a0b8f4fd
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-04-26 17:32:39 +0900

    お気に入り登録機能実装（4/26グループワーク）

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-  #resources :blogs
-  get '/blogs', to: 'blogs#top'
+  resources :blogs
+  #get '/blogs', to: 'blogs#top'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
+  resources :favorites, only: [:create, :destroy]
+  root to: "favorites#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_26_045658) do
+ActiveRecord::Schema.define(version: 2021_04_26_065538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "blog_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -29,4 +38,5 @@ ActiveRecord::Schema.define(version: 2021_04_26_045658) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "blogs", "users"
 end
