ddf6aef2088a4b5ceccc9b3e3412c3a19bf1ac65
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-04-26 14:13:38 +0900

    ログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   #resources :blogs
   get '/blogs', to: 'blogs#top'
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
 end
